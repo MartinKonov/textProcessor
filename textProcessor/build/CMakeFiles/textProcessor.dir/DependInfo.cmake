3edf1da50d4e1d37001db0df6fb9b90297d83573
MartinKonovL <martokonov@gmail.com>
2025-05-24 21:44:46 +0300

    changed folder structure

--- textProcessor/build/CMakeFiles/textProcessor.dir/DependInfo.cmake
+++ textProcessor/build/CMakeFiles/textProcessor.dir/DependInfo.cmake
@@ -9,7 +9,9 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/main.cpp" "CMakeFiles/textProcessor.dir/main.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/main.cpp.o.d"
-  "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/Documents/Document.cpp" "CMakeFiles/textProcessor.dir/src/Documents/Document.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/Documents/Document.cpp.o.d"
+  "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/Document/Document.cpp" "CMakeFiles/textProcessor.dir/src/Document/Document.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/Document/Document.cpp.o.d"
+  "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/Document/DocumentParser.cpp" "CMakeFiles/textProcessor.dir/src/Document/DocumentParser.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/Document/DocumentParser.cpp.o.d"
+  "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/Document/DocumentRegister.cpp" "CMakeFiles/textProcessor.dir/src/Document/DocumentRegister.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/Document/DocumentRegister.cpp.o.d"
   "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/FileManager.cpp" "CMakeFiles/textProcessor.dir/src/FileManager.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/FileManager.cpp.o.d"
   "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/Lines/Line.cpp" "CMakeFiles/textProcessor.dir/src/Lines/Line.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/Lines/Line.cpp.o.d"
   "/home/mkonov/cProjects/UNI/projectOOP/textProcessor/textProcessor/src/Lines/LineCreator.cpp" "CMakeFiles/textProcessor.dir/src/Lines/LineCreator.cpp.o" "gcc" "CMakeFiles/textProcessor.dir/src/Lines/LineCreator.cpp.o.d"
