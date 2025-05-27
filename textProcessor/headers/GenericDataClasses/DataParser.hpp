40f413cf83e51ff3a3634caee19e86fa4969fef0
MartinKonovL <martokonov@gmail.com>
2025-05-27 21:19:34 +0300

    Added comments for doxygen on the generic classes

--- textProcessor/headers/GenericDataClasses/DataParser.hpp
+++ textProcessor/headers/GenericDataClasses/DataParser.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file DataParser.hpp
+ * @brief A template class for parsing data items.
+ * This class provides methods to save and load data items from a file.
+ * It is designed to be extended by derived classes that implement specific serialization and parsing logic.
+ */
+
 #pragma once
 #include <vector>
 #include <string>
@@ -10,6 +17,12 @@ using std::vector;
 template <typename Derived, typename T>
 class DataParser {
 public:
+    /**
+     * @brief Saves a single item to the data file.
+     * @param item Pointer to the item to be saved.
+     * @throws std::runtime_error if there is an error during file operations.
+     * @details This method appends the serialized item to the data file.
+    */
     void save(T* item) {
         string content = ((Derived*)this)->serialize(item);
 
@@ -31,6 +44,12 @@ public:
         }
     }
 
+    /**
+     * @brief Saves multiple items to the data file.
+     * @param items Vector of pointers to items to be saved.
+     * @throws std::runtime_error if there is an error during file operations.
+     * @details This method concatenates the serialized items and saves them to the data file.
+    */
     void save(const vector<T*>& items) {
         string content;
         for (T* item : items) {
@@ -46,6 +65,12 @@ public:
         }
     }
 
+    /**
+     * @brief Loads items from the data file.
+     * @return A vector of pointers to the loaded items.
+     * @throws std::runtime_error if there is an error during file operations or parsing.
+     * @details This method reads the content of the data file, splits it into lines, and parses each line into an item.
+    */
     vector<T*> load() {
         vector<T*> items;
         try {
@@ -77,6 +102,13 @@ protected:
         fileManager = FileManager::getInstance();
     }
 
+    /**
+     * @brief Splits a string into a vector of strings based on a delimiter.
+     * @param str The string to be split.
+     * @param delimiter The character used to split the string.
+     * @return A vector of strings resulting from the split operation.
+     * @details This method is used to parse lines from the data file.
+    */
     vector<string> split(const string& str, char delimiter) {
         vector<string> result;
         string current;
--- textProcessor/headers/GenericDataClasses/DataRegister.hpp
+++ textProcessor/headers/GenericDataClasses/DataRegister.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file DataRegister.hpp
+ * @brief A template class for managing a collection of items with a specific data type.
+ * This class provides methods to add, retrieve, remove, and display items, as well as to parse and serialize them using a specified parser type.
+ */
 #pragma once
 #include "../FileManager.hpp"
 #include <stdexcept>
@@ -6,11 +11,20 @@ template <typename Derived, typename T, typename ParserType>
 class DataRegister {
  public:
 
+    /**
+     * @brief Returns all items in the register.
+     * @return A vector of pointers to all items.
+     */
     vector<T*> getAll() const {
         return items;
     }
 
-
+    /**
+     * @brief Returns an item by its name from the register.
+     * @param name The name of the item to retrieve.
+     * @return A pointer to the item if found.
+     * @throws std::runtime_error if the name is empty or the item is not found.
+     */
     T* getItem(const string& name) const {
         if (name.empty()) {
             throw std::runtime_error(Derived::getType() + "::getItem: name is empty");
@@ -22,6 +36,11 @@ class DataRegister {
         return items[index];
     }
 
+    /**
+     * @brief Removes an item by its name from the register.
+     * @param name The name of the item to remove.
+     * @throws std::runtime_error if the name is empty or there is an error during file operations.
+     */
     void removeItem(const string& name) {
         if (name.empty()) {
             throw std::runtime_error(Derived::getType() + "::removeItem: name is empty");
@@ -42,6 +61,10 @@ class DataRegister {
         }
     }
 
+    /**
+     * @brief Shows all items in the register as a concatenated string.
+     * @return A string containing the serialized representation of all items.
+     */
     string showAll() const {
         string out;
         for (T* item : items) {
@@ -55,6 +78,11 @@ class DataRegister {
     ParserType* parser;
     vector<T*> items;
 
+    /**
+     * @brief Constructor for DataRegister.
+     * Initializes the parser and loads items from the data file.
+     * @throws std::runtime_error if there is an error during file operations.
+     */
     DataRegister() {
         parser = ParserType::getInstance();
         try {
@@ -64,6 +92,10 @@ class DataRegister {
         }
     }
 
+    /**
+     * @brief Destructor for DataRegister.
+     * Cleans up the items in the register.
+     */
     ~DataRegister() {
         for (T* item : items) {
             delete item;
@@ -71,6 +103,11 @@ class DataRegister {
         items.clear();
     }
 
+    /**
+     * @brief Finds the index of an item by its name.
+     * @param name The name of the item to find.
+     * @return The index of the item if found, otherwise -1.
+     */
     int findIndex(const string& name) const {
         for (size_t i = 0; i < items.size(); ++i) {
             if (items[i]->getName() == name) {
@@ -80,6 +117,12 @@ class DataRegister {
         return -1;
     }
 
+    /**
+     * @brief Adds a new item to the register.
+     * @param newItem A pointer to the item to be added.
+     * @throws std::runtime_error if the name is empty or an item with the same name already exists.
+     * @details This method adds a new item to the register and saves it using the parser.
+     */
     void addItem(T* newItem) {
         const string name = Derived::getItemName(newItem);
         if (name.empty()) {;
